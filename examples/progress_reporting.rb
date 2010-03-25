06e3fd8469d313ab8eb961c7b06e3516c036ac9f
Ominiom <iain.iw.wilson@googlemail.com>
2010-03-25 19:43:58 +0000

    Added progress reporting example

---
+++ examples/progress_reporting.rb
@@ -0,0 +1,26 @@
+Transcode do
+
+  input '../spec/samples/sample.ogv'
+
+  overwrite!
+
+  video do
+    codec :flv
+    resolution 320, 240
+    bitrate '240k'
+  end
+
+  audio do
+    codec :libmp3lame
+    bitrate '64k'
+  end
+
+  output 'test.flv'
+
+  log 'simple.log'
+
+  event :progress do |transcode, progress|
+    puts "#{progress[:percent]}%"
+  end
+
+end
