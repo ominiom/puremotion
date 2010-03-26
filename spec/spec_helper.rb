2380a4b7903a3121b531ea5e3e19ade76b49d75b
Ominiom <iain.iw.wilson@googlemail.com>
2010-03-26 09:45:17 +0000

    Added media reading tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,7 @@ require 'puremotion'
 def sample_path
   ::File.expand_path(::File.join(::File.dirname(__FILE__), "samples", "sample.ogv"))
 end
+
+def invalid_file
+  ::File.expand_path(::File.join(::File.dirname(__FILE__), "samples", "invalid.txt"))
+end
--- spec/units/media_spec.rb
+++ spec/units/media_spec.rb
@@ -16,13 +16,60 @@ describe "media" do
       }.should raise_exception(ArgumentError)
     end
 
+    it "should handle invalid files" do
+      lambda {
+        Media(invalid_file)
+      }.should raise_exception(PureMotion::UnsupportedFormat)
+    end
+
   end
 
   context "reading media" do
 
+    before(:all) do
+      @media = Media(sample_path)
+    end
+
     it "can check for streams" do
-      media = Media(sample_path)
-      media.video?.should be_true and media.audio?.should be_true
+      @media.video?.should be_true and @media.audio?.should be_true
+    end
+
+    it "can read video resolution" do
+      @media.video.resolution.should == [320, 176]
+    end
+
+    it "can detect stream types" do
+      @media.video.type.should == :video and
+        @media.audio.type.should == :audio
+    end
+
+    it "can read the duration" do
+      @media.video.duration.should be_close(55, 3)
+    end
+
+    it "can seek reasonably accurately" do
+      @media.video.seek 5
+      @media.video.position.should be_close(5, 1)
+    end
+
+    after(:all) do
+      @media = nil
+    end
+
+  end
+
+  context "grabbing" do
+
+    before(:all) do
+      @media = Media(sample_path)
+    end
+
+    it "can grab a video frame" do
+      @media.video.grab.should be_an_instance_of(PureMotion::Frame)
+    end
+
+    after(:all) do
+      @media = nil
     end
 
   end
