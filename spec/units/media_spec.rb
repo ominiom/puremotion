f917ca2471ce816b3b839a415a714e20489de4ba
Ominiom <iain.iw.wilson@googlemail.com>
2010-03-26 09:28:21 +0000

    Added some more tests

--- spec/units/media_spec.rb
+++ spec/units/media_spec.rb
@@ -1,13 +1,30 @@
 require File.dirname(__FILE__) + '/../spec_helper.rb'
 
-describe PureMotion::Media do
+describe "media" do
 
-  it "should read supported formats" do
-    lambda do
-      PureMotion::Media.new(sample_path)
-    end.should_not raise_error
+  context "opening media" do
+
+    it "should read supported formats" do
+      lambda do
+        PureMotion::Media.new(sample_path)
+      end.should_not raise_error
+    end
+
+    it "should handle file not found" do
+      lambda {
+        Media('a_file_that_does_not_exist')
+      }.should raise_exception(ArgumentError)
+    end
+
   end
 
+  context "reading media" do
 
+    it "can check for streams" do
+      media = Media(sample_path)
+      media.video?.should be_true and media.audio?.should be_true
+    end
+
+  end
 
 end
